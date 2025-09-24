cfe69170d7d8093f0cdd1cf9f0526544d77d31f3
mounika391 <mounikayeadkey@gmail.com>
2025-09-24 12:45:38 +0530

    Add files via upload

---
+++ PROJECT/StudentMain.java
@@ -0,0 +1,13 @@
+package Student;
+import java.util.Scanner;
+import java.io.*;
+class StudentMain implements Operations{
+	public static void main(String...args){
+	String name, id, branch, password;
+	Scanner st = new Scanner(Sytem.in);
+	public void Register(Student std){
+		System.out.println("Welcome to Registration");
+	
+
+	}
+]
